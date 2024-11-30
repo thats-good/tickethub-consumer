cfe5a588961c9be23db15336b3a17aaac6550e06
imscow11253 <seaweed8759@naver.com>
2024-12-01 02:28:27 +0900

    git init

---
+++ src/main/java/com/example/tickethub_consumer/TickethubConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tickethub_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickethubConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickethubConsumerApplication.class, args);
+	}
+
+}
